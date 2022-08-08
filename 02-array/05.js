4f89c281508714a366fcb4fc4a0631d290f70115
mnxmnz <codi0703@naver.com>
2022-08-09 02:39:15 +0900

    08.08 배열 탐색 Q.5 수정

--- 02-array/05.js
+++ 02-array/05.js
@@ -1,29 +1,7 @@
 const arr = [87, 89, 92, 100, 76];
 
 function solution(arr) {
-  const rank = [...arr];
-  rank.sort((a, b) => a - b);
-
-  const len = arr.length;
-
-  const answer = Array.from({ length: len }, () => 0);
-
-  for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len; j++) {
-      if (rank[i] === arr[j]) answer[j] = i + 1;
-    }
-  }
-
-  return answer;
-}
-
-console.log(solution(arr));
-
-// 강사님 풀이
-
-function solution(arr) {
   let n = arr.length;
-
   let answer = Array.from({ length: n }, () => 1);
 
   for (let i = 0; i < n; i++) {
@@ -34,3 +12,5 @@ function solution(arr) {
 
   return answer;
 }
+
+console.log(solution(arr));
