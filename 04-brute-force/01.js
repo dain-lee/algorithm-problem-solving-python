82d138c6d83b31a53eeaae587b3793ea300da8de
mnxmnz <codi0703@naver.com>
2022-08-18 02:58:08 +0900

    08.17 브루트 포스 Q.1 수정

--- 04-brute-force/01.js
+++ 04-brute-force/01.js
@@ -1,12 +1,39 @@
 const arr = [128, 460, 603, 40, 521, 137, 123];
 
 function solution(arr) {
-	let answer = Number.MIN_SAFE_INTEGER;
-	let max = Number.MIN_SAFE_INTEGER;
+	let max = 0;
+	let answer = 0;
 
 	for (let x of arr) {
-		let sum = 0;
-		let tmp = x;
+		const arr = x
+			.toString()
+			.split('')
+			.map(x => Number(x));
+
+		const sum = arr.reduce((a, b) => a + b);
+
+		if (max < sum) {
+			max = sum;
+			answer = x;
+		} else if (max === sum) {
+			if (answer < x) answer = x;
+		}
+	}
+
+	return answer;
+}
+
+console.log(solution(arr));
+
+// 강사님 풀이
+
+function solution(arr) {
+	let answer,
+		max = Number.MIN_SAFE_INTEGER;
+
+	for (let x of arr) {
+		let sum = 0,
+			tmp = x;
 
 		while (tmp) {
 			sum += tmp % 10;
@@ -23,5 +50,3 @@ function solution(arr) {
 
 	return answer;
 }
-
-console.log(solution(arr));
