ea5a101ccde05183eac86856514f6f569690bba0
mnxmnz <codi0703@naver.com>
2022-08-18 02:58:23 +0900

    08.17 브루트 포스 Q.2

---
+++ 04-brute-force/02.js
@@ -0,0 +1,25 @@
+const arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
+
+function isPrime(n) {
+	if (n === 1) return false;
+
+	for (let i = 2; i < Math.sqrt(n); i++) {
+		if (n % i === 0) return false;
+	}
+
+	return true;
+}
+
+function solution(arr) {
+	let answer = [];
+
+	for (let x of arr) {
+		const reversedNum = Number(x.toString().split('').reverse().join(''));
+
+		if (isPrime(reversedNum)) answer.push(reversedNum);
+	}
+
+	return answer;
+}
+
+console.log(solution(arr));
